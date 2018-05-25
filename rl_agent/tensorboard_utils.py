4de3e7d16b1ea860198c2df198c71b51bb5005f3
Anton Potapchuk <antpotapchuk@gmail.com>
2018-05-25 23:28:10 +0300

    tensorboard utils added add histogram method

--- rl_agent/tensorboard_utils.py
+++ rl_agent/tensorboard_utils.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-
+import numpy as np
 
 def create_summary_writer(logdir):
     return tf.summary.FileWriter(logdir)
@@ -17,3 +17,35 @@ def add_summaries(writer, tags, values, step, prefix=''):
     for (t, v) in zip(tags, values):
         s = create_summary(prefix + t, v)
         writer.add_summary(s, global_step=step)
+
+def add_histogram(writer, tag, values, step, bins=1000):
+	"""Logs the histogram of a list/vector of values."""
+	# Convert to a numpy array
+	values = np.array(values)
+
+	# Create histogram using numpy
+	counts, bin_edges = np.histogram(values, bins=bins)
+
+	# Fill fields of histogram proto
+	hist = tf.HistogramProto()
+	hist.min = float(np.min(values))
+	hist.max = float(np.max(values))
+	hist.num = int(np.prod(values.shape))
+	hist.sum = float(np.sum(values))
+	hist.sum_squares = float(np.sum(values ** 2))
+
+	# Requires equal number as bins, where the first goes from -DBL_MAX to bin_edges[1]
+	# See https://github.com/tensorflow/tensorflow/blob/master/tensorflow/core/framework/summary.proto#L30
+	# Thus, we drop the start of the first bin
+	bin_edges = bin_edges[1:]
+
+	# Add bin edges and counts
+	for edge in bin_edges:
+		hist.bucket_limit.append(edge)
+	for c in counts:
+		hist.bucket.append(c)
+
+	# Create and write Summary
+	summary = tf.Summary(value=[tf.Summary.Value(tag=tag, histo=hist)])
+	writer.add_summary(summary, step)
+	writer.flush()
