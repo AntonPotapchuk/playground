a86a5e9f5363ab14b8d6b42e99ea3a3704175672
Anton Potapchuk <antpotapchuk@gmail.com>
2018-05-27 00:00:04 +0300

    MCTS

--- rl_agent/mcts_agent5.py
+++ rl_agent/mcts_agent5.py
@@ -2,33 +2,34 @@ import argparse
 import multiprocessing
 from queue import Empty
 import random
-import time
+import keras
 import os
 import re
 import glob
 import pickle
 import numpy as np
+import time
 
 import pommerman
 from pommerman.agents import BaseAgent, SimpleAgent
 from pommerman import constants
 
-from keras.models import Model, load_model, model_from_json
-from keras.layers import Input, Conv2D, Flatten, Dense
+from keras.models import Model, model_from_json
+from keras.layers import Input, Flatten, Dense, Convolution2D, BatchNormalization, Activation, Add
 from keras.callbacks import EarlyStopping
 import keras.backend as K
 import tensorflow as tf
 
-
 NUM_AGENTS = 4
 NUM_ACTIONS = len(constants.Action)
+
 
 class MCTSNode(object):
     def __init__(self, p):
         # values for 6 actions
         self.Q = np.zeros(NUM_ACTIONS)
         self.W = np.zeros(NUM_ACTIONS)
-        self.N = np.zeros(NUM_ACTIONS)
+        self.N = np.zeros(NUM_ACTIONS, dtype=np.uint32)
         assert p.shape == (NUM_ACTIONS,)
         self.P = p
 
@@ -50,6 +51,7 @@ class MCTSNode(object):
         else:
             Nt = self.N ** (1.0 / temperature)
             return Nt / np.sum(Nt)
+
 
 class MCTSAgent(BaseAgent):
     def __init__(self, model_file=None, train=False, agent_id=0):
@@ -82,6 +84,7 @@ class MCTSAgent(BaseAgent):
         self.hit_probs = []
         self.avg_lengths = []
         self.entropies = []
+        self.iters_sec = []
 
     def observation_to_features(self, obs):
         # TODO: history of n moves?
@@ -123,23 +126,23 @@ class MCTSAgent(BaseAgent):
         hits = 0
         misses = 0
         total_length = 0
+        start_time = time.time()
         for i in range(num_iters):
             # restore game state to root node
             obs = self.env.reset()
-            #print('\rStep %d: iteration %d' % (self.env._step_count, i + 1), end=' ')
+            print('\rStep %d: iteration %d' % (self.env._step_count, i + 1), end=' ')
             # serialize game state
             state = str(self.env.get_json_info())
 
             trace = []
             done = False
             while not done:
-                #print(board)
                 if state in self.tree:
                     node = self.tree[state]
                     # choose actions based on Q + U
                     action = node.action()
                     trace.append((node, action))
-                    #print("Action from tree:", constants.Action(action).name)
+                    # print("Action from tree:", constants.Action(action).name)
                     hits += 1
                 else:
                     # initialize action probabilities with policy network
@@ -147,22 +150,19 @@ class MCTSAgent(BaseAgent):
                     feats = feats[np.newaxis, ...]
                     probs, values = self.model.predict(feats)
                     probs = probs[0]
-                    values = values[0]
+                    reward = values[0, 0]
 
                     # add Dirichlet noise to root node for added exploration
-                    if len(trace) == 0:
-                        noise = np.random.dirichlet([args.mcts_dirichlet_alpha] * len(probs))
-                        probs = (1 - args.mcts_dirichlet_epsilon) * probs + args.mcts_dirichlet_epsilon * noise
+                    # Hex people didn't find it necessary
+                    # if len(trace) == 0:
+                    #    noise = np.random.dirichlet([args.mcts_dirichlet_alpha] * len(probs))
+                    #    probs = (1 - args.mcts_dirichlet_epsilon) * probs + args.mcts_dirichlet_epsilon * noise
 
                     # add new node to the tree
                     self.tree[state] = MCTSNode(probs)
 
-                    # get a reward
-                    rewards = self.env._get_rewards()
-                    reward = rewards[self.agent_id]
-
                     misses += 1
-                    #print("Leaf node")
+                    # print("Leaf node")
                     # stop at leaf node
                     break
 
@@ -175,24 +175,29 @@ class MCTSAgent(BaseAgent):
                 # step environment forward
                 obs, rewards, done, info = self.env.step(actions)
                 reward = rewards[self.agent_id]
+                # print("Rewards:", rewards)
 
                 state = str(self.env.get_json_info())
 
             total_length += len(trace)
 
-            #print("Finished rollout, length:", len(trace))
-            #print("Backpropagating rewards:", rewards)
+            # print("Finished rollout, length:", len(trace))
+            # print("Backpropagating reward:", reward)
 
             # update tree nodes with rollout results
-            for node, action in trace:
+            for node, action in reversed(trace):
                 node.update(action, reward)
+                reward *= args.discount
 
-            #print("Root Q:")
-            #print(self.tree[root].Q)
-            #print("Root N:")
-            #print(self.tree[root].N)
+            # print("Root Q:")
+            # print(self.tree[root].Q)
+            # print("Root N:")
+            # print(self.tree[root].N)
+            print(self.tree[root].N, self.tree[root].Q, end='')
 
-        #print("(tree hits: %0.2f, avg. len: %0.2f, tree size: %d)" % (hits / (hits + misses), total_length / num_iters, len(self.tree)))
+        # print("(tree hits: %0.2f, avg. len: %0.2f, tree size: %d)" % (hits / (hits + misses), total_length / num_iters, len(self.tree)))
+        elapsed = time.time() - start_time
+        self.iters_sec.append(num_iters / elapsed)
         self.hit_probs.append(hits / (hits + misses))
         self.avg_lengths.append(total_length / num_iters)
 
@@ -201,6 +206,7 @@ class MCTSAgent(BaseAgent):
         self.env._init_game_state = None
         # return action probabilities
         pi = self.tree[root].probs(temperature)
+        print()
         idx = (pi != 0)
         self.entropies.append(-np.sum(pi[idx] * np.log(pi[idx])))
         return pi
@@ -224,11 +230,11 @@ class MCTSAgent(BaseAgent):
             self.model.set_weights(pickle.loads(shared_buffer.raw))
 
             # use temperature 1 for first 30 steps and temperature 0 afterwards
-            temp = 1 #if self.env._step_count < 30 else 0
+            # temp = 0 if self.env._step_count < 30 else 0
             # TODO: only works when agent has access to the env
             root = self.env.get_json_info()
             # do Monte-Carlo tree search
-            pi = self.search(root, args.mcts_iters, temp)
+            pi = self.search(root, args.mcts_iters, args.temperature)
             # sample action from probabilities
             action = np.random.choice(NUM_ACTIONS, p=pi)
             # record observations and action probabilities
@@ -244,12 +250,15 @@ class MCTSAgent(BaseAgent):
             # step environment
             obs, rewards, done, info = self.env.step(actions)
             assert self == self.env._agents[self.agent_id]
-            print("Agent:", self.agent_id, "Step:", self.env._step_count, "Actions:", [constants.Action(a).name for a in actions], "Entropy: %.2f" % self.entropies[-1], "Rewards:", rewards, "Done:", done)
+            print("Agent:", self.agent_id, "Step:", self.env._step_count, "Actions:",
+                  [constants.Action(a).name for a in actions], "Probs:", [round(p, 2) for p in pi],
+                  "Entropy: %.2f" % self.entropies[-1], "Iters/s: %.2f" % self.iters_sec[-1], "Rewards:", rewards,
+                  "Done:", done)
 
-            #print("Rollout finished:", finished.value)
+            # print("Rollout finished:", finished.value)
 
         reward = rewards[self.agent_id]
-        #print("Agent:", self.agent_id, "Reward:", reward, "Len trace:", len(trace))
+        # print("Agent:", self.agent_id, "Reward:", reward, "Len trace:", len(trace))
         return trace, reward, rewards
 
     def act(self, obs, action_space):
@@ -259,7 +268,7 @@ class MCTSAgent(BaseAgent):
         probs = probs[0]
         return np.argmax(probs)
         # sample action from probabilities
-        #return np.random.choice(NUM_ACTIONS, p=pi)
+        # return np.random.choice(NUM_ACTIONS, p=pi)
 
 
 class ReplayMemory(object):
@@ -283,24 +292,55 @@ class ReplayMemory(object):
         return self.observations[:self.count], self.action_probs[:self.count], self.state_values[:self.count]
 
 
-def make_model():
-    c = x = Input(shape=(constants.BOARD_SIZE, constants.BOARD_SIZE, 17))
-    for i in range(args.conv_layers):
-        c = Conv2D(args.conv_filters, args.conv_filter_size, activation='relu', padding='valid')(c)
-    h = Flatten()(c)
-    for i in range(args.hidden_layers):
-        h = Dense(args.hidden_nodes, activation='relu')(h)
-    hp = h
-    for i in range(args.policy_hidden_layers):
-        hp = Dense(args.hidden_nodes, activation='relu')(hp)
-    p = Dense(6, activation='softmax', name="policy")(hp)
-    hv = h
-    for i in range(args.value_hidden_layers):
-        hv = Dense(args.hidden_nodes, activation='relu')(hv)
-    v = Dense(1, activation='tanh', name="value")(hv)
-    model = Model(x, [p, v])
+def get_res_block(input):
+    # Res block 1
+    x = Convolution2D(256, 3, padding='same')(input)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = Convolution2D(256, 3, padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Add()([input, x])
+    x = Activation('relu')(x)
+    return x
+
+
+def make_model(actions=6, input_shape=(constants.BOARD_SIZE, constants.BOARD_SIZE, 17)):
+    inp = Input(input_shape)
+    x = Convolution2D(256, 3, padding='same')(inp)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    # Ten residual blocks
+    for i in range(3):
+        x = get_res_block(x)
+
+    # Output block
+    # Should be 2 filters
+    x = Convolution2D(4, 1, padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = Flatten()(x)
+
+    probs = Dense(actions, activation='softmax')(x)
+    reward = Dense(128, activation='relu', name='reward_1')(x)
+    reward = Dense(1, activation='tanh', name='reward_out')(reward)
+
+    model = Model(inputs=inp, outputs=[probs, reward])
     model.compile(optimizer='adam', loss=['categorical_crossentropy', 'mse'])
     return model
+
+
+def load_model(model_path):
+    model = keras.models.load_model(model_path)
+    inp = model.layers[0]
+    probabilities = model.layers[-1].output
+    h = model.layers[-2]
+    reward = Dense(128, activation='relu', name='reward_1')(h.output)
+    reward = Dense(1, activation='tanh', name='reward_out')(reward)
+    model = Model(inp.input, [probabilities, reward])
+    model.compile(optimizer='adam', loss=['categorical_crossentropy', 'mse'])
+    return model
+
 
 def init_tensorflow():
     # make sure TF does not allocate all memory
@@ -309,7 +349,10 @@ def init_tensorflow():
     config.gpu_options.allow_growth = True
     K.set_session(tf.Session(config=config))
 
+
 def runner(id, model_file, shared_buffer, fifo, finished, _args):
+    import sys
+    #sys.stdin = open("/dev/stdin", "r")
     # make args accessible to MCTSAgent
     global args
     args = _args
@@ -326,20 +369,24 @@ def runner(id, model_file, shared_buffer, fifo, finished, _args):
         if finished.value:
             break
         # add data samples to training set
-        fifo.put((trace, reward, rewards, agent_id, agent.hit_probs, agent.avg_lengths, len(agent.tree), agent.entropies))
-        #print("Runner finished:", finished.value)
+        fifo.put((
+                 trace, reward, rewards, agent_id, agent.hit_probs, agent.avg_lengths, len(agent.tree), agent.entropies,
+                 agent.iters_sec))
+        # print("Runner finished:", finished.value)
 
-    #print("Runner done")
+    # print("Runner done")
+
 
 def trainer(num_episodes, fifos, shared_buffer, model, memory, writer):
-    callbacks = [EarlyStopping(monitor='loss', min_delta=0.01, patience=5, verbose=1, mode='auto')]
+    callbacks = [EarlyStopping(monitor='val_loss', min_delta=0.001, patience=5, verbose=1, mode='auto')]
     while num_episodes < args.num_episodes:
         while True:
             # pick random fifo (agent)
             fifo = random.choice(fifos)
             try:
                 # wait for a new trajectory and statistics
-                trace, reward, rewards, agent_id, hit_probs, avg_lengths, tree_size, entropies = fifo.get(timeout=args.queue_timeout)
+                trace, reward, rewards, agent_id, hit_probs, avg_lengths, tree_size, entropies, iters_sec = fifo.get(
+                    timeout=args.queue_timeout)
                 # break out of the infinite loop
                 break
             except Empty:
@@ -356,6 +403,7 @@ def trainer(num_episodes, fifos, shared_buffer, model, memory, writer):
         add_summary(writer, "tree/size", tree_size, num_episodes)
         add_summary(writer, "tree/mean_hit_prob", float(np.mean(hit_probs)), num_episodes)
         add_summary(writer, "tree/mean_rollout_len", float(np.mean(avg_lengths)), num_episodes)
+        add_summary(writer, "tree/iters_sec", float(np.mean(iters_sec)), num_episodes)
         add_histogram(writer, "tree/hit_probability", hit_probs, num_episodes)
         add_histogram(writer, "tree/rollout_length", avg_lengths, num_episodes)
         add_histogram(writer, "tree/entropies", entropies, num_episodes)
@@ -369,17 +417,18 @@ def trainer(num_episodes, fifos, shared_buffer, model, memory, writer):
         add_summary(writer, "replay_memory/count", memory.count, num_episodes)
         add_summary(writer, "replay_memory/current", memory.current, num_episodes)
 
-        #print("Replay memory size: %d, count: %d, current: %d" % (memory.size, memory.count, memory.current))
+        # print("Replay memory size: %d, count: %d, current: %d" % (memory.size, memory.count, memory.current))
         X, y, z = memory.dataset()
         assert len(X) != 0
 
         # reset weights?
         if args.reset_network:
-            #model.set_weights(init_weights)
+            # model.set_weights(init_weights)
             model = model_from_json(model.to_json())
             model.compile(optimizer='adam', loss=['categorical_crossentropy', 'mse'])
         # train for limited epochs to avoid overfitting?
-        history = model.fit(X, [y, z], batch_size=args.batch_size, epochs=args.num_epochs, callbacks=callbacks)
+        history = model.fit(X, [y, z], batch_size=args.batch_size, epochs=args.num_epochs, callbacks=callbacks,
+                            validation_split=args.validation_split)
         # log loss values
         for k, v in history.history.items():
             add_summary(writer, "training/" + k, v[-1], num_episodes)
@@ -407,20 +456,16 @@ if __name__ == "__main__":
     # trainer params
     parser.add_argument('--batch_size', type=int, default=128)
     parser.add_argument('--num_epochs', type=int, default=100)
+    parser.add_argument('--validation_split', type=float, default=0.1)
     parser.add_argument('--reset_network', action='store_true', default=False)
-    # network params
-    parser.add_argument('--conv_layers', type=int, default=0)
-    parser.add_argument('--conv_filters', type=int, default=32)
-    parser.add_argument('--conv_filter_size', type=int, default=3)
-    parser.add_argument('--hidden_layers', type=int, default=0)
-    parser.add_argument('--hidden_nodes', type=int, default=32)
-    parser.add_argument('--policy_hidden_layers', type=int, default=0)
-    parser.add_argument('--value_hidden_layers', type=int, default=0)
     # MCTS params
     parser.add_argument('--mcts_iters', type=int, default=10)
     parser.add_argument('--mcts_c_puct', type=float, default=1.0)
     parser.add_argument('--mcts_dirichlet_epsilon', type=float, default=0.25)
     parser.add_argument('--mcts_dirichlet_alpha', type=float, default=0.3)
+    # RL params
+    parser.add_argument('--discount', type=float, default=0.99)
+    parser.add_argument('--temperature', type=float, default=0)
     args = parser.parse_args()
 
     memory = ReplayMemory()
@@ -475,6 +520,7 @@ if __name__ == "__main__":
         process.start()
 
     from tensorboard_utils import create_summary_writer, add_summary, add_histogram
+
     writer = create_summary_writer(logdir)
 
     # do training in main process
--- rl_agent/tensorboard_utils.py
+++ rl_agent/tensorboard_utils.py
@@ -8,9 +8,42 @@ def create_summary_writer(logdir):
 def create_summary(tag, value):
     return tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value)])
 
+# https://stackoverflow.com/questions/42012906/create-a-custom-tensorflow-histogram-summary
+def create_histogram(tag, values, bins=100):
+    # Convert to a numpy array
+    values = np.array(values)
+
+    # Create histogram using numpy
+    counts, bin_edges = np.histogram(values, bins=bins)
+
+    # Fill fields of histogram proto
+    hist = tf.HistogramProto()
+    hist.min = float(np.min(values))
+    hist.max = float(np.max(values))
+    hist.num = int(np.prod(values.shape))
+    hist.sum = float(np.sum(values))
+    hist.sum_squares = float(np.sum(values**2))
+
+    # Requires equal number as bins, where the first goes from -DBL_MAX to bin_edges[1]
+    # See https://github.com/tensorflow/tensorflow/blob/master/tensorflow/core/framework/summary.proto#L30
+    # Thus, we drop the start of the first bin
+    bin_edges = bin_edges[1:]
+
+    # Add bin edges and counts
+    for edge in bin_edges:
+        hist.bucket_limit.append(edge)
+    for c in counts:
+        hist.bucket.append(c)
+
+    # Return Summary
+    return tf.Summary(value=[tf.Summary.Value(tag=tag, histo=hist)])
 
 def add_summary(writer, tag, value, step):
     writer.add_summary(create_summary(tag, value), global_step=step)
+
+
+def add_histogram(writer, tag, values, step, bins=100):
+    writer.add_summary(create_histogram(tag, values, bins), global_step=step)
 
 
 def add_summaries(writer, tags, values, step, prefix=''):
@@ -18,34 +51,3 @@ def add_summaries(writer, tags, values, step, prefix=''):
         s = create_summary(prefix + t, v)
         writer.add_summary(s, global_step=step)
 
-def add_histogram(writer, tag, values, step, bins=1000):
-	"""Logs the histogram of a list/vector of values."""
-	# Convert to a numpy array
-	values = np.array(values)
-
-	# Create histogram using numpy
-	counts, bin_edges = np.histogram(values, bins=bins)
-
-	# Fill fields of histogram proto
-	hist = tf.HistogramProto()
-	hist.min = float(np.min(values))
-	hist.max = float(np.max(values))
-	hist.num = int(np.prod(values.shape))
-	hist.sum = float(np.sum(values))
-	hist.sum_squares = float(np.sum(values ** 2))
-
-	# Requires equal number as bins, where the first goes from -DBL_MAX to bin_edges[1]
-	# See https://github.com/tensorflow/tensorflow/blob/master/tensorflow/core/framework/summary.proto#L30
-	# Thus, we drop the start of the first bin
-	bin_edges = bin_edges[1:]
-
-	# Add bin edges and counts
-	for edge in bin_edges:
-		hist.bucket_limit.append(edge)
-	for c in counts:
-		hist.bucket.append(c)
-
-	# Create and write Summary
-	summary = tf.Summary(value=[tf.Summary.Value(tag=tag, histo=hist)])
-	writer.add_summary(summary, step)
-	writer.flush()
